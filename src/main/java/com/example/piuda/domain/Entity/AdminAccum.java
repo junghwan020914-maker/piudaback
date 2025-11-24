0f9f8809bc25643a98deb166211b0b9f1bcf0075
juho <dbswngh4390@naver.com>
2025-11-24 23:16:17 +0900

    관리자, 단체 대시보드를 위한 데이터베이스 누적값(폐기물 비율 그래프) 추가

--- src/main/java/com/example/piuda/domain/Entity/AdminAccum.java
+++ src/main/java/com/example/piuda/domain/Entity/AdminAccum.java
@@ -33,6 +33,33 @@ public class AdminAccum {
     @Column(name = "accum_updated_at")
     private LocalDateTime accumUpdatedAt;
 
+    @Column(name = "trash_pet")
+    private Integer accumtrashPet;
+
+    @Column(name = "trash_bag")
+    private Integer accumtrashBag;
+
+    @Column(name = "trash_net")
+    private Integer accumtrashNet;
+
+    @Column(name = "trash_glass")
+    private Integer accumtrashGlass;
+
+    @Column(name = "trash_can")
+    private Integer accumtrashCan;
+
+    @Column(name = "trash_rope")
+    private Integer accumtrashRope;
+
+    @Column(name = "trash_cloth")
+    private Integer accumtrashCloth;
+
+    @Column(name = "trash_elec")
+    private Integer accumtrashElec;
+
+    @Column(name = "trash_etc")
+    private Integer accumtrashEtc;
+
     @ManyToOne
     @JoinColumn(name = "admin_id", nullable = false)
     private Admin admin;
